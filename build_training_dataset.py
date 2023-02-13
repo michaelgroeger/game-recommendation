ced72dfa3dc1b2d3e1687118b5d06369ad283e8e
michaelgroeger <michael.groeger@posteo.net>
2023-02-13 15:25:24 +0100

    Add promptin to dataset creation

--- build_training_dataset.py
+++ build_training_dataset.py
@@ -4,16 +4,18 @@
 #####################################
 import argparse
 
-from cloudgaming.data_processors.Dataset import build_training_datasets
+from data_processors.Dataset import build_training_datasets
 
 parser = argparse.ArgumentParser(description="Create training datasets")
 parser.add_argument("-n", "--n_highest", type=int, help="Select n most played games")
+parser.add_argument("--prompts", action="store_true")
+parser.add_argument("--no-prompts", dest="prompts", action="store_false")
 args = parser.parse_args()
 
 
 def main():
     subset_game_information, subset_user_game_matrix = build_training_datasets(
-        args.n_highest, add_prompts=False
+        args.n_highest, add_prompts=args.prompts
     )
     print(
         f"Created new datasets with {len(subset_game_information)} games and {len(subset_user_game_matrix)} users."
